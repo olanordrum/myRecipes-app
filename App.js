20547c52f91b774af478d35d0b7602291989c4cf
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2026-03-30 16:24:24 +0200

    Add dependencies and set up initial navigation

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+import Home from './screens/Home';
+import CreateRecipe from './screens/CreateRecipe';
+import Profile from "./screens/Profile";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Home', tabBarIcon: () => <Ionicons name="home" size={20} />, tabBarAccessibilityLabel: 'Homescreen tab' }}
+        />
+        <Tab.Screen
+          name="Create recipe"
+          component={CreateRecipe}
+          options={{ title: 'Create Recipe', tabBarIcon: () => <Ionicons name="pencil" size={20} />, tabBarAccessibilityLabel: 'Create recipe tab' }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{ title: 'Profile', tabBarIcon: () => <Ionicons name="person" size={20} />, tabBarAccessibilityLabel: 'Profile tab' }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Home.js
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>
+                HOME
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
